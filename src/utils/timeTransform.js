c205a97a79587d06acaf8362dc162ba882754cf7
Nguyen Huu Trong <trongnh2003@gmail.com>
2023-09-04 10:14:36 +0700

    update

--- src/utils/timeTransform.js
+++ src/utils/timeTransform.js
@@ -1,9 +1,5 @@
 export const datetimeTransform = (datetime, type = "all") => {
-  const [date, time] = datetime.split(" ");
-  const [year, month, day] = date.split("-");
-  const [hour, minute, second] = time.split(":");
-
-  const transform = new Date(Date.UTC(year, month, day, hour, minute, second));
+  const transform = new Date(datetime);
 
   switch (type) {
     case "all":
@@ -24,8 +20,9 @@ export const datetimeTransform = (datetime, type = "all") => {
       });
     case "time":
       return transform.toLocaleString("en-US", {
-        hour: "2-digit",
         minute: "2-digit",
+        hour: "2-digit",
+        hour12: true,
       });
   }
 };
