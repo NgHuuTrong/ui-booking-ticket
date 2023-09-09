23ca9d4bc3aa15a77bf30aa5696922a258765514
Trần Hải Nam <steven.hai.ta856@gmail.com>
2023-09-09 17:04:44 +0700

    improve performance Match Screen
    change from scrollview to flatlist

--- src/screens/MatchScreen.js
+++ src/screens/MatchScreen.js
@@ -1,54 +1,45 @@
-import {
-  Pressable,
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { MainLayout } from "../components/Common/MainLayout";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { themeColors } from "../theme";
-import { MatchCard } from "../components/Matches/MatchCard";
 import { DateSection } from "../components/Matches/DateSection";
 import { AxiosContext } from "../services/axios.context";
-import axios from "axios";
 import { datetimeTransform } from "../utils/timeTransform";
 import { getAllMatches } from "../services/match.service";
 import InAppLoading from "../components/InAppLoading";
+import { FlatList } from "react-native";
 
 export const MatchScreen = () => {
   const { authAxios, publicAxios } = useContext(AxiosContext);
-  const [ref, setRef] = useState(null);
-  const [tapRef, setTapRef] = useState(null);
+  const tapRef = useRef(null);
+  const sectionRef = useRef(null);
   const [matchDataGroupByDate, setMatchDataGroupByDate] = useState([]);
-  const [index, setIndex] = useState(0);
-  const [sectionCords, setSectionCords] = useState([]);
-  const [tapCords, setTapCords] = useState([]);
+  const [sectionIndex, setSectionIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
-  const scrollHandler = (e) => {
-    setIndex(e);
-    ref.scrollTo({
-      x: 0,
-      y: sectionCords[e].pos,
-      animated: true,
-    });
-  };
+
+  //get and work with data from backend
   useEffect(() => {
     const fetchMatches = async () => {
       let data = await getAllMatches(authAxios);
       const sortedData = data.sort(
         (data1, data2) => new Date(data1.time) - new Date(data2.time)
       );
-      const dataGroupByDate = {};
+      const dataGroupByDate = [];
       sortedData.forEach((match) => {
         const date = datetimeTransform(match.time, "CustomForMatchScreen");
-        if (!dataGroupByDate[date]) {
-          dataGroupByDate[date] = [];
+        if (
+          dataGroupByDate.length == 0 ||
+          dataGroupByDate[dataGroupByDate.length - 1].date !== date
+        ) {
+          dataGroupByDate.push({
+            date: date,
+            matchesByDate: [],
+          });
         }
-        dataGroupByDate[date].push(match);
+        dataGroupByDate[dataGroupByDate.length - 1].matchesByDate.push(match);
       });
-      for (const [key, value] of Object.entries(dataGroupByDate)) {
-        const roundsMap = value.reduce((map, match) => {
+      dataGroupByDate.forEach((data, index) => {
+        const roundsMap = data.matchesByDate.reduce((map, match) => {
           const { round } = match;
           if (!map.has(round)) {
             map.set(round, []);
@@ -65,118 +56,107 @@ export const MatchScreen = () => {
           round,
           matches,
         }));
-        dataGroupByDate[key] = roundsArray;
-      }
+        dataGroupByDate[index].matchesByDate = roundsArray;
+      });
       setMatchDataGroupByDate(dataGroupByDate);
+      setIsLoading(false);
     };
     fetchMatches();
   }, []);
-  useEffect(() => {
-    const dataLength = Object.keys(matchDataGroupByDate).length;
-    if (
-      sectionCords.length > 0 &&
-      tapCords.length > 0 &&
-      sectionCords.length === dataLength &&
-      tapCords.length === dataLength
-    ) {
-      setIsLoading(false);
-    }
-  }, [sectionCords, tapCords]);
-  const handleScroll = (event) => {
-    const scrollY = event.nativeEvent.contentOffset.y;
-    for (let i = 0; i < sectionCords.length; i++) {
-      if (
-        scrollY > sectionCords[i].pos - 100 &&
-        scrollY < sectionCords[i].pos + sectionCords[i].offset - 100
-      ) {
-        setIndex((prev) => i);
-        tapRef.scrollTo({
-          x: tapCords[i],
-          y: 0,
-          animated: true,
-        });
-        break;
-      }
+
+  //handle scroll top tap and section to index
+  const handleScrollToIndex = (index) => {
+    setSectionIndex(index);
+    tapRef.current?.scrollToIndex({
+      index,
+      animated: true,
+      viewPosition: 0.1,
+    });
+    sectionRef.current?.scrollToIndex({
+      index,
+      animated: true,
+    });
+  };
+
+  //config for visible item detect
+  viewabilityConfig = {
+    waitForInteraction: true,
+    itemVisiblePercentThreshold: 80,
+  };
+  onViewableItemsChanged = ({ viewableItems, changed }) => {
+    if (viewableItems && viewableItems.length > 0) {
+      const currentIndex = viewableItems[0].index;
+      setSectionIndex(currentIndex);
+      tapRef.current?.scrollToIndex({
+        index: currentIndex,
+        animated: true,
+        viewPosition: 0.1,
+      });
     }
   };
+  const viewabilityConfigCallbackPairs = useRef([
+    { viewabilityConfig, onViewableItemsChanged },
+  ]);
 
   return (
     <MainLayout>
-      <View className="h-14 mt-2">
-        <ScrollView
-          horizontal
-          contentContainerStyle={{ alignItems: "center" }}
-          ref={(ref) => {
-            setTapRef(ref);
-          }}
-        >
-          {index >= 0 &&
-            Object.keys(matchDataGroupByDate).map((date, i) => {
-              return (
-                <Pressable
-                  style={
-                    index == i
-                      ? {
-                          backgroundColor: themeColors.bgButton,
-                        }
-                      : {
-                          backgroundColor: "transparent",
-                          borderWidth: 1,
-                          borderColor: themeColors.bgButton,
-                        }
-                  }
-                  className="h-9 items-center justify-center ml-2 p-2 rounded-lg"
-                  key={i}
-                  onPress={() => scrollHandler(i)}
-                  onLayout={(event) => {
-                    const layout = event.nativeEvent.layout;
-                    tapCords[i] = layout.x;
-                    setTapCords([...tapCords]);
+      <View className="h-14 mt-2 flex-1">
+        <View>
+          <FlatList
+            horizontal
+            data={matchDataGroupByDate}
+            ref={tapRef}
+            renderItem={({ item, index }) => (
+              <Pressable
+                style={
+                  index == sectionIndex
+                    ? {
+                        backgroundColor: themeColors.bgButton,
+                      }
+                    : {
+                        backgroundColor: "transparent",
+                        borderWidth: 1,
+                        borderColor: themeColors.bgButton,
+                      }
+                }
+                className="h-9 items-center justify-center ml-2 p-2 rounded-lg"
+                onPress={() => handleScrollToIndex(index)}
+              >
+                <Text
+                  style={{
+                    color:
+                      index == sectionIndex
+                        ? themeColors.bgScreen
+                        : themeColors.bgButton,
                   }}
+                  className="font-bold "
                 >
-                  <Text
-                    style={{
-                      color:
-                        index == i
-                          ? themeColors.bgScreen
-                          : themeColors.bgButton,
-                    }}
-                    className="font-bold "
-                  >
-                    {date}
-                  </Text>
-                </Pressable>
-              );
-            })}
-        </ScrollView>
+                  {item.date}
+                </Text>
+              </Pressable>
+            )}
+            keyExtractor={(item) => item.date}
+          />
+        </View>
+        <View className="flex-1 p-4">
+          <FlatList
+            ref={sectionRef}
+            data={matchDataGroupByDate}
+            viewabilityConfigCallbackPairs={
+              viewabilityConfigCallbackPairs.current
+            }
+            renderItem={({ item, index }) => (
+              <View>
+                <DateSection
+                  date={item.date}
+                  matchesByDate={item.matchesByDate}
+                ></DateSection>
+              </View>
+            )}
+            keyExtractor={(item) => item.date}
+          />
+        </View>
       </View>
-      <ScrollView
-        ref={(ref) => {
-          setRef(ref);
-        }}
-        className="flex-1 px-2"
-        style={{ backgroundColor: themeColors.bgScreen }}
-        contentContainerStyle={{ paddingBottom: 2 }}
-        onScroll={handleScroll}
-      >
-        {Object.keys(matchDataGroupByDate).map((date, index) => {
-          return (
-            <View
-              key={index}
-              onLayout={(event) => {
-                const layout = event.nativeEvent.layout;
-                sectionCords[index] = { pos: layout.y, offset: layout.height };
-                setSectionCords([...sectionCords]);
-              }}
-            >
-              <DateSection
-                date={date}
-                matchesByDate={matchDataGroupByDate[date]}
-              ></DateSection>
-            </View>
-          );
-        })}
-      </ScrollView>
       <InAppLoading visible={isLoading}></InAppLoading>
     </MainLayout>
   );
